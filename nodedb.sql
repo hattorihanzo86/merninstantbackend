53a206b9cda3aee86db3697ef9edc3e71cfd0b88
Andrian R <andrianreyes@gmail.com>
2022-10-01 13:27:41 +0800

    Here is your simple and easy server

---
+++ nodedb.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 01, 2022 at 05:02 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nodedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `createdAt` date DEFAULT NULL,
+  `updatedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,28 @@
+**NodeJS Express Backend  for Easy Development**
+
+ - Clone the repository first.
+    `git clone https://github.com/hattorihanzo86/merninstantbackend.git`
+
+ - Install dependencies 
+ `npm install`
+
+ - Create Database name it **nodedb**
+- Import the database **nodedb.sql** you can find it here in the root folder.
+
+**Configuration and File Structure**
+
+ **Database** 	- config/ database.js 
+**Controllers** - controller/ products.js 
+**Models**    - models/ productModel.js 
+**Routes**    - routes/ index.js 
+
+Running server type the  `nodemon index`
+
+just incase nodemon is not found 
+you can install the package globally `npm install -g nodemon`
+
+If you found out that his is very useful pls buy me a coffee  :coffee:
+https://www.buymeacoffee.com/areyes1986
+
+
+
